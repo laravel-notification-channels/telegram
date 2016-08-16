384d89afadf1426610d2e259a5afcd37815bf113
Mohamed Said <themohamedsaid@gmail.com>
2016-08-16 02:10:49 +0200

    remove events since they are now fired in laravel core

--- src/TelegramChannel.php
+++ src/TelegramChannel.php
@@ -3,8 +3,6 @@
 namespace NotificationChannels\Telegram;
 
 use Illuminate\Notifications\Notification;
-use NotificationChannels\Telegram\Events\MessageWasSent;
-use NotificationChannels\Telegram\Events\SendingMessage;
 use NotificationChannels\Telegram\Exceptions\CouldNotSendNotification;
 
 class TelegramChannel
@@ -32,10 +30,6 @@ class TelegramChannel
      */
     public function send($notifiable, Notification $notification)
     {
-        if (!$this->shouldSendMessage($notifiable, $notification)) {
-            return;
-        }
-
         $message = $notification->toTelegram($notifiable);
 
         if (is_string($message)) {
@@ -53,20 +47,5 @@ class TelegramChannel
         $params = $message->toArray();
 
         $this->telegram->sendMessage($params);
-
-        event(new MessageWasSent($notifiable, $notification));
-    }
-
-    /**
-     * Check if we can send the notification.
-     *
-     * @param              $notifiable
-     * @param Notification $notification
-     *
-     * @return bool
-     */
-    protected function shouldSendMessage($notifiable, Notification $notification)
-    {
-        return event(new SendingMessage($notifiable, $notification), [], true) !== false;
     }
 }
