1b36763b2f506c8cf52cd5d20c04b015cc1440f7
Zulkifli Mohamed <mr.putera@gmail.com>
2021-06-08 08:47:38 +0800

    fixed styleci

--- src/TelegramServiceProvider.php
+++ src/TelegramServiceProvider.php
@@ -3,9 +3,9 @@
 namespace NotificationChannels\Telegram;
 
 use GuzzleHttp\Client as HttpClient;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Notifications\ChannelManager;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\ServiceProvider;
 
 /**
  * Class TelegramServiceProvider.
