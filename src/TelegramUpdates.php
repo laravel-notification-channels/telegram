f9dc7e09ae099bd728289258fe7255f07b763ca8
faissaloux <fwahabali@gmail.com>
2021-11-24 22:22:46 +0100

    remove TelegramUpdates useless implementations

--- src/TelegramUpdates.php
+++ src/TelegramUpdates.php
@@ -2,16 +2,11 @@
 
 namespace NotificationChannels\Telegram;
 
-use JsonSerializable;
-use NotificationChannels\Telegram\Traits\HasSharedLogic;
-
 /**
  * Class TelegramUpdates.
  */
-class TelegramUpdates implements JsonSerializable
+class TelegramUpdates
 {
-    use HasSharedLogic;
-
     public static function create(): self
     {
         return new self();
@@ -31,9 +26,7 @@ class TelegramUpdates implements JsonSerializable
 
     public function get(): array
     {
-        $response = (new Telegram())->setToken(config('services.telegram-bot-api.token'))
-            ->getUpdates($this->payload)
-        ;
+        $response = (new Telegram())->setToken(config('services.telegram-bot-api.token'))->getUpdates($this->payload);
 
         return json_decode($response->getBody()->getContents(), true);
     }
