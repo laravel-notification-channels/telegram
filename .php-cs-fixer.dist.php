d7766a77326c640f587a2245a838ebe5aaddebdc
Irfaq Syed <irazasyed@users.noreply.github.com>
2021-11-24 14:19:08 +0530

    Rename as per latest changes.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,6 +3,7 @@
 $finder = PhpCsFixer\Finder::create()->in(__DIR__);
 
 return PhpCsFixer\Config::create()
+    ->setRiskyAllowed(true)
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
