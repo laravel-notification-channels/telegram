9b999bf7f134bc67ecf94c05eb163fb44d4a6a48
Soheil Best <soheilbest@yahoo.com>
2023-06-07 16:40:38 +0330

    Update add_two_factor_columns_to_model_table.php

--- database/migrations/add_two_factor_columns_to_model_table.php
+++ database/migrations/add_two_factor_columns_to_model_table.php
@@ -1,1 +1,24 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->bigInteger("chat_bale_id")->nullable()->default(null);
+            $table->string("secret_bot", 75)->nullable()->default(null);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumns("chat_bale_id");
+            $table->dropColumns("secret_bot");
+        });
+    }
+};
